04d1ac5b43ef9217338e1aa0a37159470f4de96e
Kayla Reopelle <kreopelle@gmail.com>
2018-04-10 10:16:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -81,5 +81,7 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index| element + "s"}
+  array.each_with_index.collect do |element, index|
+    if element[index] != element[1]
+    element + "s"
 end
