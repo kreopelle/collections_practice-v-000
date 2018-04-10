9730c3de220d4b7bab43f40664ad7969450e58e5
Kayla Reopelle <kreopelle@gmail.com>
2018-04-10 09:32:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(int_array)
+  int_array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      1
+    elsif a < b
+      -1
+    end
+  end
+end
+
+def sort_array_desc(int_array)
+  int_array.sort do |a, b|
+    if a == b
+      0
+    elsif a > b
+      -1
+    elsif a < b
+      1
+    end
+  end
+end
+
+def sort_array_char_count(str_array)
+  str_array.sort do |a,b|
+    if a.length == b.length
+      0
+    elsif a.length > b.length
+      1
+    elsif a.length < b.length
+      -1
+    end
+  end
+end
+
+def swap_elements(array)
+  item_1 = array[1]
+  item_2 = array[2]
+  array[2] = item_2
+  array[1] = item_1
+end
+
 
